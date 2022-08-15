8ccb560040f8ccf1ace829b277e7600442840d46
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-15 12:00:10 -0300

    base url on tsconfig

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { ReactComponent as Logo } from 'assets/logo.svg';
 
 function Cardapio() {
   return (
