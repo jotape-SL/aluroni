49faa7b2c9f72eda3abe0c1d81d106041e2b405d
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-17 03:24:01 -0300

    style

---
+++ src/components/Ordenador/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Ordenador.module.scss';
+import opcoes from '../Filtros/filtros.json';
+
+function Ordenador() {
+  return (
+    <button>
+      <span></span>
+      <div></div>
+    </button>
+  );
+}
+
+export default Ordenador;
