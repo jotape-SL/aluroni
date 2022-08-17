d92d26c5d7342f3ec4d8f51f31019e17eb557135
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-17 03:52:18 -0300

    making it open and adding icons

--- src/components/Ordenador/index.tsx
+++ src/components/Ordenador/index.tsx
@@ -1,11 +1,27 @@
 import styles from './Ordenador.module.scss';
 import opcoes from './opscoes.json';
+import { useState } from 'react';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 function Ordenador() {
+  const [aberto, setAberto] = useState(false);
   return (
-    <button className={styles.ordenador}>
+    <button
+      className={styles.ordenador}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
       <span>Ordenar Por</span>
-      <div className={styles.ordenador__options}>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={`${styles.ordenador__options} ${
+          aberto && styles['ordenador__options--ativo']
+        }`}
+      >
         {opcoes.map((opcao) => (
           <div className={styles.ordenador__option} key={opcao.value}>
             {opcao.nome}
