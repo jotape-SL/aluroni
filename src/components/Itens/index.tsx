98560d1d6278e50797cba5b136fed0b17977bb37
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-19 11:44:40 -0300

    buscador e filtro

--- src/components/Itens/index.tsx
+++ src/components/Itens/index.tsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react';
 import Item from './Item';
 import cardapio from './Itens.json';
 import styles from './Itens.module.scss';
 
-function Itens() {
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: String;
+}
+
+function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro } = props;
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true;
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+    setLista(novaLista);
+  }, [lista]);
+
   return (
     <div>
-      {cardapio.map((item) => {
+      {lista.map((item) => {
         return (
           <div className={styles.itens}>
             <Item key={item.id} {...item} />
